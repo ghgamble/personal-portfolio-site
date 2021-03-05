a7611d95211ade08fc0d6ca27d549832bcd2ab22
Grace Gamble <grace.gamble@lifeomic.com>
2021-03-05 12:34:43 -0500

    fix domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         typeName: `WPgraphql`,
         fieldName: `wpgraphql`,
-        url: `http://grace-gamble.com/graphql`,
+        url: `http://testgatsbywp.local/graphql`,
       },
     },
     {
